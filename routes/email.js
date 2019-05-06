7f64e9e11d8cf48f8901f77de2663cd08a3f1784
Jake Waltrip <jake.waltrip@gmail.com>
2019-05-05 22:46:49 -0500

    almost done with getting the server ready to migrate to other repo

--- routes/email.js
+++ routes/email.js
@@ -6,6 +6,15 @@ const mail = require("../utils/sendEmail");
 router.post('/email/send', (req, res, next) => {
   const { email, name, subject, message } = req.body;
   
+  // return an error if any of these parameters are missing or empty
+  if  (!email || !name || !subject || !message) {
+    return res.json({
+      sucess: false,
+      error: "You must provide info for Email, Name, Subject and Message fields"
+    });
+  }
+  
+  // send the email
   mail.sendEmail(email, name, subject, message)
     .then(result => {
       // log the result of sending the email and email details
